5c08153a71ed3d411b7ae0fdabbeb207bd11eb84
Maxime Chasles <maxime.chasles@etudiant.univ-lr.fr>
2025-10-25 10:31:22 +0200

    refactor: move styles in stylesheet

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,10 @@ import PWABadge from "./PWABadge.tsx";
 import "./App.css";
 import Map from "react-map-gl/mapbox";
 import "mapbox-gl/dist/mapbox-gl.css";
-import { useState, useCallback, useEffect } from "react";
-import { useNavigatorGeolocation } from "./hooks/useNavigatorGeolocation.ts";
-import CurrentPosition from "./components/CurrentPosition.tsx";
+import { useState } from "react";
+import CurrentPosition from "./components/CurrentLocation.tsx";
 import { useBluetooth } from "./contexts/BluetoothContext.tsx";
 import ZoneGeometry from "./components/ZoneGeometry.tsx";
-
-type MapCamera = {
-  longitude: number;
-  latitude: number;
-  zoom: number;
-};
 
 const MAPBOX_ACCESS_TOKEN = import.meta.env.VITE_MAPBOX_ACCESS_TOKEN;
 
@@ -22,38 +15,17 @@ if (!MAPBOX_ACCESS_TOKEN) {
   );
 }
 
-function App() {
-  console.debug("___Render: App");
+const App = () => {
+  console.log("App rendered");
+  const [isMapLoaded, setIsMapLoaded] = useState<boolean>(false);
+
   const { bluetoothConnected, connectBluetooth, disconnectBluetooth } =
     useBluetooth();
-
-  const [mapLoaded, setMapLoaded] = useState<boolean>(false);
-  const { currentLocation } = useNavigatorGeolocation();
-
-  const [mapCamera, setMapCamera] = useState<MapCamera>({
-    longitude: -1.1517,
-    latitude: 46.1591,
-    zoom: 12,
-  });
-
-  // Centrer la carte sur la position actuelle quand elle est obtenue
-  useEffect(() => {
-    if (currentLocation) {
-      setMapCamera({
-        longitude: currentLocation[0],
-        latitude: currentLocation[1],
-        zoom: 14,
-      });
-    }
-  }, [currentLocation]);
-
-  const handleMapLoaded = useCallback(() => {
-    setMapLoaded(true);
-  }, []);
 
   return (
     <div className="map-container">
       <Map
+        id="map"
         mapboxAccessToken={MAPBOX_ACCESS_TOKEN}
         interactive
         initialViewState={{
@@ -61,40 +33,14 @@ function App() {
           latitude: 46.1591,
           zoom: 10,
         }}
-        viewState={{
-          longitude: mapCamera.longitude,
-          latitude: mapCamera.latitude,
-          zoom: mapCamera.zoom,
-          bearing: 0,
-          pitch: 0,
-          padding: {
-            top: 0,
-            bottom: 0,
-            left: 0,
-            right: 0,
-          },
-          width: 0,
-          height: 0,
-        }}
         mapStyle="mapbox://styles/vincentlb/cmfwcr7jb008101sc99fk3cfo"
         attributionControl={false}
-        onLoad={handleMapLoaded}
+        onLoad={() => setIsMapLoaded(true)}
         onMove={(evt) => {
           console.log("Map moved:", evt.viewState);
-          setMapCamera((prev) => ({
-            ...prev,
-            longitude: evt.viewState.longitude,
-            latitude: evt.viewState.latitude,
-            zoom: evt.viewState.zoom,
-          }));
-        }}
-        style={{
-          width: "100%",
-          height: "100%",
-          boxSizing: "border-box",
         }}
       >
-        {mapLoaded && (
+        {isMapLoaded && (
           <>
             <header className="header">
               <h1>NavX</h1>
@@ -112,7 +58,7 @@ function App() {
               </div>
             </header>
 
-            <CurrentPosition position={currentLocation} />
+            <CurrentPosition />
             <ZoneGeometry />
           </>
         )}
@@ -121,6 +67,6 @@ function App() {
       <PWABadge />
     </div>
   );
-}
+};
 
 export default App;
