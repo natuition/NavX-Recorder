5daa0431c034c1c21d3e5d3dabf29d4cc50cb42a
Maxime Chasles <maxime.chasles@etudiant.univ-lr.fr>
2025-10-25 10:30:06 +0200

    refactor: rename component

---
+++ src/components/CurrentLocation.tsx
@@ -0,0 +1,60 @@
+import type { FeatureCollection, Point } from "geojson";
+import { Layer, Source, useMap } from "react-map-gl/mapbox";
+import type { CircleLayerSpecification } from "react-map-gl/mapbox";
+import { useNavigatorGeolocation } from "../hooks/useNavigatorGeolocation";
+import { useEffect } from "react";
+
+const CurrentLocation = () => {
+  const { currentLocation } = useNavigatorGeolocation();
+  const { current: map } = useMap();
+
+  // Centrer la carte sur la position actuelle quand elle est obtenue
+  useEffect(() => {
+    if (!map) return;
+    if (currentLocation) {
+      map.flyTo({
+        center: [currentLocation[0], currentLocation[1]],
+        zoom: 14,
+        speed: 1.2,
+      });
+    }
+  }, [currentLocation, map]);
+
+  // Créer les données GeoJSON pour la position actuelle
+  const currentLocationGeoJSON: FeatureCollection<Point> = {
+    type: "FeatureCollection",
+    features: currentLocation
+      ? [
+          {
+            type: "Feature",
+            properties: { current: true },
+            geometry: {
+              type: "Point",
+              coordinates: currentLocation,
+            },
+          },
+        ]
+      : [],
+  };
+
+  const layerSpecifications: CircleLayerSpecification = {
+    id: "current-location",
+    type: "circle",
+    paint: {
+      "circle-color": "#2a5ebe",
+      "circle-opacity": 0.8,
+      "circle-radius": 8,
+      "circle-stroke-color": "#ffffff",
+      "circle-stroke-width": 3,
+    },
+    source: "current-location",
+  };
+
+  return (
+    <Source id="current-location" type="geojson" data={currentLocationGeoJSON}>
+      <Layer {...layerSpecifications} />
+    </Source>
+  );
+};
+
+export default CurrentLocation;
