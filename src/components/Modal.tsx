23c8981ecd61f29a6e436c60d168ec28177c4e44
Maxime Chasles <maxime.chasles@etudiant.univ-lr.fr>
2025-10-31 17:07:46 +0100

    feat: add modal confirmation BLE disconnect

---
+++ src/components/Modal.tsx
@@ -0,0 +1,25 @@
+import React, { type ReactNode, useEffect } from "react";
+
+type ModalProps = {
+  isOpen: boolean;
+  onClose: () => void;
+  children: ReactNode;
+};
+
+export const Modal = ({ isOpen, onClose, children }: ModalProps) => {
+  if (!isOpen) return null;
+
+  return (
+    <div className="modal-overlay" onClick={onClose}>
+      <div
+        className="modal-content"
+        onClick={(e) => e.stopPropagation()} // Empêche la fermeture en cliquant à l’intérieur
+      >
+        <button className="modal-close" onClick={onClose}>
+          ✕
+        </button>
+        {children}
+      </div>
+    </div>
+  );
+};
--- src/layouts/MapLayout.tsx
+++ src/layouts/MapLayout.tsx
@@ -1,5 +1,5 @@
-import Map from "react-map-gl/mapbox";
-import { useState, type ReactNode } from "react";
+import Map, { useMap } from "react-map-gl/mapbox";
+import { useEffect, useState, type ReactNode } from "react";
 import {
   MdOutlineBluetooth,
   MdOutlineBluetoothConnected,
@@ -7,35 +7,45 @@ import {
 import { useBluetooth } from "../contexts/BluetoothContext";
 import CurrentLocation from "../components/CurrentLocation";
 import { GeolocationProvider } from "../contexts/GeolocationContext";
+import { useModal } from "../hooks/useModal";
+import { Modal } from "../components/Modal";
+import { useNavigatorGeolocation } from "../hooks/useNavigatorGeolocation";
 
 const MAPBOX_ACCESS_TOKEN = import.meta.env.VITE_MAPBOX_ACCESS_TOKEN;
 const MAP_INITIAL_LATITUDE = 46.1591;
 const MAP_INITIAL_LONGITUDE = -1.1517;
 
-// const Camera = () => {
-//   const { initialLocation } = useNavigatorGeolocation();
+const Camera = () => {
+  const { initialLocation } = useNavigatorGeolocation();
 
-//   const { current: map } = useMap();
+  const { current: map } = useMap();
 
-//   useEffect(() => {
-//     if (!map) return;
+  useEffect(() => {
+    if (!map) return;
 
-//     if (initialLocation) {
-//       map.flyTo({
-//         center: [initialLocation[0], initialLocation[1]],
-//         zoom: 14,
-//         speed: 1.2,
-//       });
-//     }
-//   }, [initialLocation, map]);
+    if (initialLocation) {
+      map.flyTo({
+        center: [initialLocation[0], initialLocation[1]],
+        zoom: 14,
+        speed: 1.2,
+      });
+    }
+  }, [initialLocation, map]);
 
-//   return <></>;
-// };
+  return <></>;
+};
 
 export const MapLayout = ({ children }: { children: ReactNode }) => {
   const [isMapLoaded, setIsMapLoaded] = useState(false);
   const { bluetoothConnected, connectBluetooth, disconnectBluetooth } =
     useBluetooth();
+
+  const modal = useModal();
+
+  const handleBluetoothDisconnect = async () => {
+    await disconnectBluetooth();
+    modal.close();
+  };
 
   return (
     <div className="map-container">
@@ -45,7 +55,7 @@ export const MapLayout = ({ children }: { children: ReactNode }) => {
         initialViewState={{
           longitude: MAP_INITIAL_LONGITUDE,
           latitude: MAP_INITIAL_LATITUDE,
-          zoom: 1.5,
+          zoom: 10,
         }}
         mapStyle="mapbox://styles/vincentlb/cmfwcr7jb008101sc99fk3cfo"
         attributionControl={false}
@@ -53,9 +63,7 @@ export const MapLayout = ({ children }: { children: ReactNode }) => {
       >
         {isMapLoaded && (
           <div
-            onClick={
-              bluetoothConnected ? disconnectBluetooth : connectBluetooth
-            }
+            onClick={bluetoothConnected ? modal.open : connectBluetooth}
             id="bluetoothControl"
           >
             {bluetoothConnected ? (
@@ -71,6 +79,14 @@ export const MapLayout = ({ children }: { children: ReactNode }) => {
             <CurrentLocation />
             {children}
           </GeolocationProvider>
+        )}
+
+        {isMapLoaded && (
+          <Modal isOpen={modal.isOpen} onClose={modal.close}>
+            <h2>Déconnecter le Bluetooth ?</h2>
+            <button onClick={handleBluetoothDisconnect}>Oui</button>
+            <button onClick={modal.close}>Non</button>
+          </Modal>
         )}
       </Map>
     </div>
