c365293e97045d9eb78236d2b3e4cb0175540b37
Maxime Chasles <maxime.chasles@etudiant.univ-lr.fr>
2025-11-26 16:32:11 +0100

    enhancement: use Marker for current position dot

--- src/components/GeolocateControl.tsx
+++ src/components/GeolocateControl.tsx
@@ -1,7 +1,5 @@
-import type { FeatureCollection, Point } from "geojson";
-import { Layer, Source, useMap } from "react-map-gl/mapbox";
-import type { CircleLayerSpecification } from "react-map-gl/mapbox";
-import { useCallback, useEffect, useState } from "react";
+import { Marker, useMap } from "react-map-gl/mapbox";
+import { useCallback, useEffect, useState, type CSSProperties } from "react";
 import { IoMdLocate } from "react-icons/io";
 import { useGeolocation } from "../hooks/useGeolocation";
 
@@ -31,28 +29,11 @@ const GeolocateControl = () => {
     if (position) {
       map.flyTo({
         center: [position.longitude, position.latitude],
-        zoom: 14,
+        zoom: 18,
         speed: 1.2,
       });
     }
   }, [map, position]);
-
-  // Créer les données GeoJSON pour la position actuelle
-  const currentLocationGeoJSON: FeatureCollection<Point> = {
-    type: "FeatureCollection",
-    features: position
-      ? [
-          {
-            type: "Feature",
-            properties: { current: true },
-            geometry: {
-              type: "Point",
-              coordinates: [position.longitude, position.latitude],
-            },
-          },
-        ]
-      : [],
-  };
 
   return (
     <>
@@ -62,28 +43,27 @@ const GeolocateControl = () => {
       >
         <IoMdLocate className="map-control__icon" />
       </div>
-      <Source
-        id="current-location"
-        type="geojson"
-        data={currentLocationGeoJSON}
-      >
-        <Layer {...layerSpecifications} />
-      </Source>
+      {isInitialCenteringDone && position && (
+        <Marker
+          style={currentPositionMarkerStyle}
+          longitude={position!.longitude}
+          latitude={position!.latitude}
+        >
+          <span className="map-control--current-position"></span>
+        </Marker>
+      )}
     </>
   );
 };
 
-const layerSpecifications: CircleLayerSpecification = {
-  id: "current-location",
-  type: "circle",
-  paint: {
-    "circle-color": "#2a5ebe",
-    "circle-opacity": 0.8,
-    "circle-radius": 8,
-    "circle-stroke-color": "#ffffff",
-    "circle-stroke-width": 3,
-  },
-  source: "current-location",
-};
-
 export default GeolocateControl;
+
+/**
+ * Style appliqué au marqueur de la position actuelle.
+ * Permet de le styliser par le biais du contenu avec une classe CSS.
+ */
+const currentPositionMarkerStyle: CSSProperties = {
+  height: "20px",
+  width: "20px",
+  position: "relative",
+};
