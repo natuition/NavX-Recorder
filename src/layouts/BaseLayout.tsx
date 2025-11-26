899fb1cca4d3afc1a647a217eade3d8e55359670
Maxime Chasles <maxime.chasles@etudiant.univ-lr.fr>
2025-11-26 09:53:32 +0100

    docs: BaseLayout

--- src/layouts/BaseLayout.tsx
+++ src/layouts/BaseLayout.tsx
@@ -1,11 +1,20 @@
 import { useLocation } from "react-router";
 import Navigation from "../components/Navigation.tsx";
 import TopBar from "../components/TopBar.tsx";
+import type { ReactNode } from "react";
 
-const BaseLayout = ({ children }: { children: React.ReactNode }) => {
+const ROUTES_WHITHOUT_NAVIGATION = ["/distance", "/area"];
+
+/**
+ * Layout de base englobant l'application avec la top bar et la navigation.
+ */
+const BaseLayout = ({ children }: { children: ReactNode }) => {
   const location = useLocation();
 
-  const showNavigation = !location.pathname.startsWith("/distance");
+  const showNavigation = !ROUTES_WHITHOUT_NAVIGATION.includes(
+    location.pathname
+  );
+
   return (
     <div className="base-layout">
       <header className="base-layout__header">
