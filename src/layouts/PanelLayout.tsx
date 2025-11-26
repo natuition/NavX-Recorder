87f8dde685a77a502c30c38c4dfe2d54aa305248
Maxime Chasles <maxime.chasles@etudiant.univ-lr.fr>
2025-11-26 09:53:54 +0100

    docs: PanelLayout

--- src/layouts/PanelLayout.tsx
+++ src/layouts/PanelLayout.tsx
@@ -1,5 +1,8 @@
 import { Outlet } from "react-router";
 
+/**
+ * Layout pour les pages ordinaires (sans carte).
+ */
 const PanelLayout = () => {
   return (
     <section className="panel-layout">
