2e00a0de82746d64b7ff45e9528cf3514e9b6b37
Maxime Chasles <maxime.chasles@etudiant.univ-lr.fr>
2025-12-01 17:40:55 +0100

    ci: update base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.GH_PAGES_BASE || '/',
   plugins: [
     react(),
     VitePWA({
