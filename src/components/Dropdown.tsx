b35cfada2104fe60d7aaa03d2078ef0164a18339
Maxime Chasles <maxime.chasles@etudiant.univ-lr.fr>
2025-12-01 11:26:48 +0100

    feat: ad ui component Dropdown

---
+++ src/components/Dropdown.tsx
@@ -0,0 +1,52 @@
+import { useState, type MouseEvent } from "react";
+import { IoMdMore } from "react-icons/io";
+
+type DropdownOption = {
+  label: string;
+  action: () => void;
+};
+
+type DropdownProps = {
+  options: DropdownOption[];
+};
+
+const Dropdown = ({ options }: DropdownProps) => {
+  const [active, setActive] = useState(false);
+
+  const handleToggle = (e: MouseEvent) => {
+    e.stopPropagation();
+    setActive(!active);
+  };
+
+  const handleClose = (e: MouseEvent) => {
+    e.stopPropagation();
+    setActive(false);
+  };
+
+  return (
+    <>
+      {active && <div onClick={handleClose} className="dropdown-overlay" />}
+      <div className="dropdown">
+        <IoMdMore className="dropdown-toggle" onClick={handleToggle} />
+        <ul
+          className={`dropdown-menu ${active ? "dropdown-menu--active" : ""}`}
+        >
+          {options?.map((option, index) => (
+            <li
+              key={index}
+              className="dropdown-item"
+              onClick={(e) => {
+                e.stopPropagation();
+                option.action();
+                setActive(false);
+              }}
+            >
+              {option.label}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
+};
+export default Dropdown;
