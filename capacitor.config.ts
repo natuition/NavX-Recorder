8ba5106babe0cca4b4fa23325556ce27168d4c39
Vincent LAMBERT <vincent.lambertvlb@gmail.com>
2025-12-04 12:51:02 +0100

    wip: ios development

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,11 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.natuition.navxrecorder',
   appName: 'navx-recorder',
-  webDir: 'dist'
+  webDir: 'dist',
+  "server": {
+    "url": "http://192.168.1.63:5173",
+    "cleartext": true
+  }
 };
 
 export default config;
