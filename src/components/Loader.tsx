a314dfef31c35ab74856583589e2381396e09136
Maxime Chasles <maxime.chasles@etudiant.univ-lr.fr>
2025-12-04 09:42:24 +0100

    feat: add Loader component

---
+++ src/components/Loader.tsx
@@ -0,0 +1,11 @@
+import { TbLoader2 } from "react-icons/tb";
+
+const Loader = () => {
+  return (
+    <div className="loader">
+      <TbLoader2 className="spin" />
+    </div>
+  );
+};
+
+export default Loader;
