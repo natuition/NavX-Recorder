a439733523f41652e33602dfba5c6f38856a04dd
Maxime Chasles <maxime.chasles@etudiant.univ-lr.fr>
2025-12-05 11:18:24 +0100

    chore(settings): remove debug section

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,13 +1,10 @@
-import Modal from "../components/Modal";
 import { useBluetooth } from "../hooks/useBluetooth";
 import { useModal } from "../hooks/useModal";
-import { useToast } from "../hooks/useToast";
 
 const Settings = () => {
   const { bluetoothConnected, connectBluetooth, disconnectBluetooth } =
     useBluetooth();
   const modal = useModal();
-  const toast = useToast();
 
   const handleNavxConnection = () => {
     if (bluetoothConnected) {
@@ -28,41 +25,6 @@ const Settings = () => {
     }
   };
 
-  const handleTestModal1 = () => {
-    modal.open({
-      message: "Hello dev, are you okay?",
-      yesLabel: true,
-      noLabel: true,
-      onYes: modal.close,
-      onNo: modal.close,
-    });
-  };
-
-  const handleTestModal2 = () => {
-    modal.open({
-      message: "Are you sure!?",
-      yesLabel: true,
-      noLabel: false,
-      onYes: modal.close,
-    });
-  };
-
-  const handleTestModal3 = () => {
-    modal.open({
-      _render: () => <Modal.SaveDistance distance={28} />,
-      yesLabel: "Cool !",
-      onYes: modal.close,
-    });
-  };
-
-  const handleTestToast1 = () => {
-    toast.info("Info toast");
-  };
-
-  const handleTestToast2 = () => {
-    toast.error("Error toast");
-  };
-
   return (
     <>
       <h1 className="page__title">Paramètres</h1>
@@ -70,25 +32,6 @@ const Settings = () => {
         <h2>Général</h2>
         <button className="button" onClick={handleNavxConnection}>
           {bluetoothConnected ? "Déconnecter" : "Connecter"} NavX
-        </button>
-      </section>
-      <section className="page__section">
-        <h2>Outils développeur</h2>
-
-        <button className="button" onClick={handleTestModal1}>
-          Test Modal 1
-        </button>
-        <button className="button" onClick={handleTestModal2}>
-          Test Modal 2
-        </button>
-        <button className="button" onClick={handleTestModal3}>
-          Test Modal 3
-        </button>
-        <button className="button" onClick={handleTestToast1}>
-          Test Toast 1
-        </button>
-        <button className="button" onClick={handleTestToast2}>
-          Test Toast 2
         </button>
       </section>
     </>
